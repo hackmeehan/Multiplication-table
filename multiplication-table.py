36a19022fa106f2408c05871e59d8d134d7a9cc7
hackmeehan <42844639+hackmeehan@users.noreply.github.com>
2018-09-17 13:07:06 -0400

    Updated from Brython Server: 9/17/2018 1:07:06 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,12 +1,37 @@
-""" multiplication-table.py 
-Author: Jack Meehan 
-Credit: To find how to get rid newline: https://gist.github.com/marcoscastro/dd176ac2b59441895369 
-Assignment: Write and submit a Python program that 
-prints a multiplication table."""
+"""
+multiplication-table.py
+Author: Jack Meehan
+Credit: To find how to get rid newline: https://gist.github.com/marcoscastro/dd176ac2b59441895369
+Assignment:
 
-width = int(input('Width of multiplication table: ')) 
-height = int(input('Height of multiplication table: ')) 
+Write and submit a Python program that prints a multiplication table. The user 
+must be prompted to give the width and height of the table before it is printed.
+
+The final multiplication table should look like this:
+
+Width of multiplication table: 10
+Height of multiplication table: 8
+
+  1   2   3   4   5   6   7   8   9  10
+  2   4   6   8  10  12  14  16  18  20
+  3   6   9  12  15  18  21  24  27  30
+  4   8  12  16  20  24  28  32  36  40
+  5  10  15  20  25  30  35  40  45  50
+  6  12  18  24  30  36  42  48  54  60
+  7  14  21  28  35  42  49  56  63  70
+  8  16  24  32  40  48  56  64  72  80
+"""
+
+width = int(input('Width of multiplication table: '))
+height = int(input('Height of multiplication table: '))
 
 for i in range(1, height+1):
+    print(' ')
     for j in range(1, width+1):
-        print(i*j , end=" ")
+        print("{0:>3}".format(str(i*j)), end=" ")
+        
+'''
+print("The number is: {0:>3}".format(1))         # Prints "The number is:   1"
+print("The number is: {0:>3}".format(12))        # Prints "The number is:  12"
+print("The number is: {0:>3}".format(123))
+'''
